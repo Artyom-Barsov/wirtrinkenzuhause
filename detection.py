77e64b307a12d65adcd0ce2f10102f762abd69b8
Artyom Barsov <42358981+Artyom-Barsov@users.noreply.github.com>
2020-05-10 13:28:37 +0300

    находим сильно связанные компоненты

--- detection.py
+++ detection.py
@@ -1,20 +1,34 @@
 import cv2
-import numpy
+import sys
+import numpy as np
+import queue
 
-def nothing(x):
-    pass
+def bfs(mask, x, y):
+    pixels = queue.Queue()
+    pixels.put((x, y))
+    while !pixels.empty():
+        newx, newy = pixels.pop()
+        if newx > 0 and newx < len(mask[0]) and newy > 0 and newy < len(mask):
+            pixels.push((newx, newy))
+
+def suchen(mask):
+    for i in range(len(mask)):
+        for j in range(len(mask[i])):
+            if mask[i][j] == 255:
+                bfs(mask, x=j, y=i)
 
 while True:
-    frame = cv2.imread('3.jpg')
-
+    frame = cv2.imread(sys.argv[1])
+    x, y = sys.argv[2], sys.argv[3]
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    l_b = numpy.array([104, 0, 0])
-    u_b = numpy.array([255, 255, 255])
+    l_b = np.array([104, 0, 0])
+    u_b = np.array([255, 255, 255])
 
     mask = cv2.inRange(hsv, l_b, u_b)
-
+    suchen(mask)
     res = cv2.bitwise_and(frame, frame, mask=mask)
+
 
     cv2.imshow("fr", frame)
     cv2.imshow("msk", mask)
